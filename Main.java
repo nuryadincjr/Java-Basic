b6f336be309bcf6e53bc468c26b31dc86809e91a
nuryadincjr <nuryadin.cjr@gmail.com>
2021-08-25 21:08:08 +0700

    declarated variable

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
+    int a, b, c;
+    int d = 10;
+    Byte B = 20;
+    double pi = 3.14;
+    char C = 'c';
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main main = new Main();
+        System.out.println(main.a);
     }
 }
