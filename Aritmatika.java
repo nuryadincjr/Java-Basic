f3f0029504a2cab2b2dc5ed58bb096d02d90a185
nuryadincjr <nuryadin.cjr@gmail.com>
2021-08-26 18:34:46 +0700

    Operator Aritmatic

---
+++ Aritmatika.java
@@ -0,0 +1,33 @@
+public class Aritmatika {
+    
+    public static void main(String[] args) {
+        // declarated variabels
+        int x = 10;
+        int y = 15;
+        int result = 0;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        result = x+y;
+        System.out.println("x + y = " + result);
+        
+        result = x-y;
+        System.out.println("x - y = " + result);
+        
+        result = x*y;
+        System.out.println("x * y = " + result);
+        
+        result = x/y;
+        System.out.println("x / y = " + result);
+        
+        result = x%y;
+        System.out.println("x % y = " + result);
+        
+        x++;
+        System.out.println("x++ = " + x);
+        
+        y--;
+        System.out.println("x-- = " + y);
+    }
+}
