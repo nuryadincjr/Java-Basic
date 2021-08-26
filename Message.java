e3036aa1ff969604b4a626261a7abdd8e0c40045
nuryadincjr <nuryadin.cjr@gmail.com>
2021-08-26 19:44:33 +0700

    Operator Intanceof

---
+++ Message.java
@@ -0,0 +1,15 @@
+public class Message {
+    /*
+    Intenceof: Keyword (instanceof) untuk membadingkan suatu objek, instansiasi dari suatu Class atau tidak dengan nilai result boolean. 
+    Pendeklarasian:  <namaObject> instanceof <NamaClass>;
+    */
+
+    String pesan = "Helo STTBandung";
+
+    public static void main(String[] args) {
+        Message message = new Message();
+
+        System.out.println(message instanceof Message); // Result: true
+        System.out.println(message.pesan instanceof String); //Result: true
+    }
+}
