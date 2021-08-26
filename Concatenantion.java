6a99297612caf039a5643fbe4a568548a93c70c0
nuryadincjr <nuryadin.cjr@gmail.com>
2021-08-26 19:52:23 +0700

    Operator Concatenantion

---
+++ Concatenantion.java
@@ -0,0 +1,11 @@
+public class Concatenantion {
+    // Konkatenansi: Penggabungan antarstring, (+).
+
+    public static void main(String[] args) {
+        String tytle = "Sila pertama ";
+        String value = "Ketuhanan yang maha Esa.";
+        String silaPertama = tytle + value;
+
+        System.out.println(silaPertama);
+    }
+}
