00e51b3731b5f7a73e65b8e80b8bb17a09b842fc
nuryadincjr <nuryadin.cjr@gmail.com>
2021-08-27 23:13:27 +0700

    Void Method

---
+++ VoidMethod.java
@@ -0,0 +1,16 @@
+public class VoidMethod {
+    /*
+    Void Method: method bertipe void dan tidak mengembalikan nilai apapun.
+    void namaMethod() {
+        // stetmen aksi
+    }
+    */
+
+    static void getURL() {
+        System.out.println("https://github.com/nuryadin.cjr");
+    }
+
+    public static void main(String[] args) {
+        getURL();
+    }
+}
